4e7dd8f9eb1b2e3c5c164fed6f7908bb1fed2d39
wjddnjs20070906-create <wjddnjs20070906@gmail.com>
2026-09-16 13:12:13 +0900

    Add files via upload

--- chapter2/09OuterJavaScript_error.js
+++ chapter2/09OuterJavaScript_error.js
@@ -1,1 +1,17 @@
-alert("Outerscript...Error";
+<!DOCTYPE html>
+<html lang="ko">
+<head>
+    <meta charset="UTF-8">
+    
+    <title>1Text Header</title>
+</head>
+<body>
+        <h1>프론트엔드 개발 기초</h1>
+        <h2>프론트엔드 개발 기초</h2>
+        <h3>프론트엔드 개발 기초</h3>
+        <h4>프론트엔드 개발 기초</h4>
+        <h5>프론트엔드 개발 기초</h5>
+        <h6>프론트엔드 개발 기초</h6>
+
+</body>
+</html>
